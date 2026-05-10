aafd9449ca8b6f7cca9092d608dcf7f7127715ca
Trần Khoa <tranmaianhkhoa2006@gmail.com>
2026-05-10 10:54:06 +0700

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s.length()==1)
+            return true;
+
+        s = s.toLowerCase();
+
+        int l = 0;
+        int r = s.length()-1;
+        while(l<r){
+            while(l<r && !checker(s.charAt(l)))
+                l++;
+
+            while(l<r && !checker(s.charAt(r)))
+                r--;    
+
+
+            if(l<r && s.charAt(l) != s.charAt(r))
+                return false;
+
+            l++;
+            r--;    
+        }
+
+        return true;
+    }
+
+    public boolean checker(char c){
+        if(Character.isLetter(c) || Character.isDigit(c) )
+            return true;
+        return false;    
+    }
+}
