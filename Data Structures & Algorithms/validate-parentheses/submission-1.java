8a14e099d10bc38b198d6085d51df9d5cfa78825
Trần Khoa <tranmaianhkhoa2006@gmail.com>
2026-05-12 09:12:39 +0700

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,42 @@
+class Solution {
+    private boolean checker(char s,char t){
+        if(Math.abs(s-t) ==2 && s<t)
+            return true;
+
+        if(Math.abs(s-t) == 1 && s<t)
+            return true;
+        return false;        
+    }
+
+    public boolean isValid(String s) {
+        if(s.length()%2!=0)
+            return false;
+
+        boolean flag1 = true;
+        boolean flag2 = true;
+        
+        int l = 0;
+        int r = s.length()-1;
+        while(l<r){
+            if( !checker(s.charAt(l),s.charAt(r)) ){
+                flag1 = false;
+                break;
+            }
+
+            l++;
+            r--;
+        }
+
+        for(int i = 0;i<s.length()-1;i+=2){
+            if( !checker(s.charAt(i),s.charAt(i+1)) ){
+                flag2 = false;
+                break;
+            }
+        }
+
+        if(flag1 || flag2)
+            return true;
+
+        return false    ;
+    }
+}
