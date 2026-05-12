4212d0040a866ff75a9c89557c59d582d9830cb9
Trần Khoa <tranmaianhkhoa2006@gmail.com>
2026-05-12 09:52:05 +0700

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.java
@@ -0,0 +1,50 @@
+class Solution {
+    private boolean checker(char s,char t){
+        if(s<t){
+            switch(Math.abs(s-t)){
+                case 2:
+                    return true;
+                case 1:
+                    return true;
+                default:
+                    return false;        
+            }
+        }
+        else return false;     
+    }
+
+    private boolean isOpen(char bracket){
+       if(bracket == '(' || bracket == '[' || bracket =='{')
+         return true;
+       return false;         
+    }
+
+    private boolean isClosed(char bracket){
+       if(bracket == ')' || bracket == ']' || bracket =='}')
+         return true;
+       return false;         
+    }
+
+    public boolean isValid(String s) {
+         if(s.length()%2!=0)
+            return false;
+
+        Stack<Character> bracket = new Stack<>();
+
+        for(int i = 0;i<s.length();i++){
+            if(isOpen(s.charAt(i))){
+                bracket.push(s.charAt(i));
+            }
+            else if(!bracket.isEmpty() && isClosed(s.charAt(i))){
+                if(!checker(bracket.pop(),s.charAt(i)))
+                    return false;
+            }
+            else return false;
+        }
+
+        if(!bracket.isEmpty())
+            return false;
+
+        return true;
+    }
+}
